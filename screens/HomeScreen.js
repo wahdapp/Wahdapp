7048fd9ae98bf0a4e12ebf1609083b7b62b1a337
aboudicheng <aboudicheng@gmail.com>
2019-10-12 19:13:12 +0300

    Enhance UX

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity, Platform } from 'react-native';
 import BottomSheet from 'reanimated-bottom-sheet';
+import * as Location from 'expo-location';
+import { Ionicons } from '@expo/vector-icons';
+import colors from '../constants/Colors';
 
 export default function HomeScreen() {
   const [selectedLocation, setSelectedLocation] = useState(null);
-  let bs = React.createRef();
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  async function getUserLocation() {
+    try {
+      const location = await Location.getCurrentPositionAsync({});
+      setCurrentRegion({
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      });
+    }
+    catch (e) {
+      throw e;
+    }
+  }
+
+  const bs = React.createRef();
 
   function handleLongPress(coords) {
     const { coordinate } = coords.nativeEvent;
     setSelectedLocation(coordinate);
     bs.current.snapTo(2);
+  }
+
+  // Google Maps only
+  function handlePoiClick(coords) {
+    const { coordinate } = coords.nativeEvent;
+    setSelectedLocation(coordinate);
+    bs.current.snapTo(2);
+  }
+
+  async function handleFloatBtnClick() {
+    bs.current.snapTo(2);
+    try {
+      const location = await Location.getCurrentPositionAsync({});
+      setSelectedLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+    }
+    catch (e) {
+      throw e;
+    }
   }
 
   const renderHeader = () => (
@@ -24,8 +66,19 @@ export default function HomeScreen() {
   const renderContent = () => (
     <View style={styles.panel}>
       <Text style={styles.panelTitle}>Confirm Prayer Information</Text>
+      <Text style={styles.panelSubtitle}>
+        People around this area will be notified afterwards
+      </Text>
     </View>
   )
+
+  if (!currentRegion) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
 
   return (
     <>
@@ -33,9 +86,23 @@ export default function HomeScreen() {
         style={{ flex: 1 }}
         showsUserLocation={true}
         onLongPress={handleLongPress}
+        onPoiClick={handlePoiClick}
+        initialRegion={{
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude
+        }}
+        showsMyLocationButton={false}
       >
         {selectedLocation && <Marker coordinate={selectedLocation} />}
       </MapView>
+      <TouchableOpacity
+        style={styles.floatingBtn}
+        onPress={handleFloatBtnClick}
+      >
+        <Ionicons name={Platform.OS === 'ios' ? `ios-add` : 'md-add'} size={30} color="#ffffff" />
+      </TouchableOpacity>
       <BottomSheet
         ref={bs}
         snapPoints={[500, 400, 250, 0]}
@@ -75,4 +142,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#00000040',
     marginBottom: 10,
   },
+  panelTitle: {
+    fontSize: 27,
+    height: 35,
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: 'gray',
+    height: 30,
+    marginBottom: 10,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  floatingBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    height: 70,
+    backgroundColor: colors.tintColor,
+    borderRadius: 100,
+  }
 })
