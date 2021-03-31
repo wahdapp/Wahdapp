67fee19f3dccfa4fc65b803347d335bb284fba65
Ping Cheng <aboudicheng@gmail.com>
2021-03-31 15:59:58 +0800

    refactor(expo): upgrade sdk to v40

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import React, { useState, useEffect } from 'react';
 import { Platform, StatusBar, StyleSheet, View, Dimensions } from 'react-native';
